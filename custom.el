235d3b5cdee34616369cb7a574056ec76f6b5122
Gen Suzuki <gen.suzuki@gmail.com>
2014-11-09 18:53:48 +0000

    Use 'Customize' to manage personal settings

---
+++ custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backup"))))
+ '(c-basic-offset 4)
+ '(c-default-style
+   (quote
+    ((c-mode . "bsd")
+     (c++-mode . "bsd")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu"))))
+ '(column-number-mode t)
+ '(fill-column 79)
+ '(global-whitespace-mode t)
+ '(indent-tabs-mode nil)
+ '(ns-right-alternate-modifier nil)
+ '(tab-width 4)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :height 140 :family "Courier")))))
---
+++ init.el
@@ -0,0 +1,3 @@
+; Keep customize settings in a separate file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
