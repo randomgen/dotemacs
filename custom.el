fffee7f13fe9d65b2f8eab3a3c54abc42202bccf
Gen Suzuki <gen.suzuki@gmail.com>
2014-12-20 21:54:33 +0000

    Change default font to Inconsolata

--- custom.el
+++ custom.el
@@ -24,4 +24,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :height 140 :family "Courier")))))
+ '(default ((t (:inherit nil :height 180 :family "Inconsolata")))))
