1d49bc589ab83b6fe8f8e63cd10aff585e9322b4
Gen Suzuki <gen.suzuki@gmail.com>
2018-05-12 18:47:25 +0100

    Disable backup files

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backup"))))
  '(c-basic-offset 4)
  '(c-default-style
    (quote
@@ -19,6 +18,7 @@
     ("9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
  '(fill-column 79)
  '(indent-tabs-mode nil)
+ '(make-backup-files nil)
  '(ns-right-alternate-modifier nil)
  '(tab-width 4)
  '(tool-bar-mode nil))
